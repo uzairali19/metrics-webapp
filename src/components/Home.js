9f23c2c7f0160556d9a68f96d3dc95b549ca60a8
Uzair Ali <uzairali19b@gmail.com>
2022-01-03 21:35:43 +0500

    update files structure

---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Home = () => {
+  const hello = 'Hello World';
+  return <div>Home Page</div>;
+};
+
+export default Home;
---
+++ src/redux/reducers/marvelReducer.js
@@ -0,0 +1,1 @@
+import { GET_ALL } from '../types';
