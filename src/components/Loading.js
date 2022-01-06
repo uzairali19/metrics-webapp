500efe43c3365d6b78d72c33dc5f7c41cf1f7580
Uzair Ali <uzairali19b@gmail.com>
2022-01-07 00:42:55 +0500

    update loading icon

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import * as React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
 
-const Loading = () => {
-  const Hello = 'Hello World';
-  return <div>Loading</div>;
-};
-
-export default Loading;
+export default function Loading() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+        flexDirection: 'column',
+      }}
+    >
+      <CircularProgress
+        sx={{
+          width: '150px !important',
+          height: '150px !important',
+          animation: 'animation-61bdi0 1.4s cubic-bezier(0.68, -0.55, 0.27, 1.55) infinite',
+        }}
+      />
+      <Typography
+        className="loading-text"
+        variant="h5"
+        component="h2"
+        sx={{
+          mt: '1rem',
+          color: '#fff',
+        }}
+      >
+        Loading ...
+      </Typography>
+    </Box>
+  );
+}
